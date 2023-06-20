90d2bd232d62eb84312d9ab8895838de68b43079
Mahamadou-Nouridine <98597449+Mahamadou-Nouridine@users.noreply.github.com>
2023-06-21 00:28:47 +0100

    Sructure rockets and setup redux

--- src/routes/Rockets.jsx
+++ src/routes/Rockets.jsx
@@ -1,7 +1,43 @@
 import React from "react";
+import { Badge, Button, Image } from "react-bootstrap";
+import styles from "./styles/Rocket.module.css";
+
+const image = "https://www.channelstv.com/wp-content/uploads/2018/03/Buhari-Gates-4.jpg";
 
 const Rockets = () => {
-  return <div>Rockets</div>;
+  return (
+    <div>
+      <div className={`${styles["rocket-container"]} row mt-3`}>
+        <div className={`${styles["image-container"]}  col-12 col-md-4`}>
+          <Image className={styles.image} src={image} />
+        </div>
+        <div className="col col-12 col-md-8">
+          <h3 className={styles.title}>Falcon 1</h3>
+          <p className={styles.description}>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo ab
+            quam culpa cupiditate a, nostrum magnam voluptas suscipit laudantium
+            officiis nam adipisci impedit voluptatem quisquam.
+          </p>
+          <Button variant="primary" className={styles.button}>Reserve Rocket</Button>
+        </div>
+      </div>
+      <div className={`${styles["rocket-container"]} row mt-3`}>
+        <div className={`${styles["image-container"]}  col-12 col-md-4`}>
+          <Image className={styles.image} src={image} />
+        </div>
+        <div className="col col-12 col-md-8">
+          <h3 className={styles.title}>Falcon 1</h3>
+          <p className={styles.description}>
+            <Badge bg="info">Reserved</Badge>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo ab
+            quam culpa cupiditate a, nostrum magnam voluptas suscipit laudantium
+            officiis nam adipisci impedit voluptatem quisquam.
+          </p>
+          <Button variant="outline-secondary" className={styles.button}>Cancel Reservation</Button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Rockets;
