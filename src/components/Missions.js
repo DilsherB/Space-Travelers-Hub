94087864732a0e7e323aadb8eadb51a55731d2a7
Dilsher <dilsherb@gmail.com>
2023-06-21 11:27:02 +0300

    data showed in mission.js

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,9 +1,18 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const Missions = () => {
+  const missions = useSelector((state) => state.missions);
   return (
     <div>
-      <h1>Missions</h1>
+      {missions.map((mission) => {
+        return (
+          <div key={mission.mission_id}>
+            <h2>{mission.mission_name}</h2>
+            <p>{mission.description}</p>
+          </div>
+        );
+      })}
     </div>
   );
 };
